2654b8afdb991fdc1284395fd059c76df154c3dd
peterturo <pst17@georgetown.edu>
2022-10-27 15:39:50 -0400

    Challenge A

--- app/unemployment.py
+++ app/unemployment.py
@@ -3,6 +3,9 @@ import json
 from pprint import pprint
 
 import requests
+from dotenv import load_dotenv # <--- ADDITION
+
+load_dotenv() # <--- ADDITION
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
@@ -14,3 +17,14 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 print(type(parsed_response))
 pprint(parsed_response)
+
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date?
+# Display the unemployment rate using a percent sign.
+
+#breakpoint()
+
+latest = parsed_response["data"][0]
+print(f"LATEST UNEMPLOYMENT RATE: {latest['value']}%, {latest['date']}")
