bb264ca79654e5b50365b9c1b0741daa0ad47ed0
peterturo <pst17@georgetown.edu>
2022-10-28 16:51:42 -0400

    CSV file

---
+++ app/csv_unemployment.py
@@ -0,0 +1,22 @@
+import os
+from pprint import pprint
+from statistics import mean
+
+from pandas import read_csv
+from dotenv import load_dotenv # <--- ADDITION
+import plotly.express as px
+
+load_dotenv() # <--- ADDITION
+
+unemployment_url_csv = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}&datatype=csv"
+
+unemployment_df = read_csv(unemployment_url_csv)
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date? 
+# Display the unemployment rate using a percent sign.
+
+latest = unemployment_df.iloc[0]
+
+print("Most Recent Unemployment Rate:", f"{latest['value']}%,", latest['timestamp'])
