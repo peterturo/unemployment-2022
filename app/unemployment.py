91e09927446c131e7cb5400666a6d561f437c09f
peterturo <pst17@georgetown.edu>
2022-10-28 16:41:51 -0400

    Challenge B-JSON

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pprint import pprint
+from statistics import mean
 
 import requests
 from dotenv import load_dotenv # <--- ADDITION
@@ -15,8 +16,8 @@ request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+#print(type(parsed_response))
+#pprint(parsed_response)
 
 
 # Challenge A
@@ -28,3 +29,24 @@ pprint(parsed_response)
 
 latest = parsed_response["data"][0]
 print(f"LATEST UNEMPLOYMENT RATE: {latest['value']}%, {latest['date']}")
+print("----------------")
+
+
+
+# Challenge B
+# 
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+unemployment_data = parsed_response["data"]
+
+cy_rates = []
+
+cy_data = [u for u in unemployment_data if "2022" in u["date"]]
+
+for c in cy_data:
+    cy_rates.append(float(c["value"]))
+
+
+print (f"AVERAGE UNEMPLOYMENT RATE IN 2022: {mean(cy_rates)}%")
+print("NO. OF MONTHS IN CALENDAR YEAR:", len(cy_rates))
